31c287d9f906de1b6de03c73216a2aa16c3f97c8
swooshnv <swoosh.nv@gmail.com>
2022-07-04 15:03:14 +0430

    Some test inputs

---
+++ inputnumbers.py
@@ -0,0 +1,7 @@
+A = [9,2,36,8,24,86,91,245,72,215,248,324,52,1,0,24,238,3,54,68,54,19,5]
+#Array "A" must be sorted completely
+B = [135,135,45,354,84,3,6,351,8,745,13,543,51,54,134521,135,43,543,1,5,15]
+#Array "B" has a number with more than 5 digits ,is an invalid array and should not be sorted
+C = [2,5442,42,42,422,72,72,728,422,638,1,8,435,-5,11]
+#Array "C" has a negative number, is an invalid array and should not be sorted
+D=[31,72,9,35,1,4,0]
