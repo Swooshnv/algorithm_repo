ea7daeb586e8821103554919179d467316f44ec9
Seyed Soroush Hosseini Naghavi <106426831+Swooshnv@users.noreply.github.com>
2022-12-25 00:53:30 +0330

    Update Knapsack.py

--- Knapsack.py
+++ Knapsack.py
@@ -3,25 +3,33 @@ import numpy as np
 def knapSack(v, w, k, W):
     cnt = len(w)
     for i in range(cnt):
-        for j in range(W - 1):
-            if i==0 or j==0:
-                k[i][j] = 0
-            elif j < w[i]:
-                k[i][j] = k[i - 1][j]
-            else:
+        for j in range(W + 1):
+            #if i==0 or j==0:
+            #    k[i][j] = 0
+            if j >= w[i]:
                 if k[i - 1][j] > k[i - 1][j - w[i]] + v[i]:
                     k[i][j] = k[i - 1][j]
                 else:
                     k[i][j] = k[i - 1][j - w[i]] + v[i]
+            else:
+                k[i][j] = k[i - 1][j]
+                
     for i in range(cnt):
-        for j in range(W - 1):
+        for j in range(W + 1):
             print(f"  {int(k[i][j])}", end = "")
         print("\n")
 
+counts = 11
+value = np.array([11], dtype = np.int32)
+weight = np.array([11], dtype = np.int32)
+print(weight.shape)
+value = [25,5,20,120,100,40,30,50,60,75,100]
+weight = [2,4,1,8,10,5,3,7,6,12,7]
+W = 30
+"""
 counts = 0
 value = np.array([0], dtype = np.int32)
 weight = np.array([0], dtype = np.int32)
-print(weight.shape)
 while(True):    
     try:    
         print(value, weight)
@@ -35,6 +43,7 @@ while(True):
         print(value, weight)
         W = int(input("Enter sack limit: "))
         break
+"""
 print(f"Item Count: {counts}, values: {value}, weight: {weight}")
-k = np.ndarray(shape = (counts, W))
+k = np.ndarray(shape = (counts + 1, W + 1))
 knapSack(value, weight, k, W)
